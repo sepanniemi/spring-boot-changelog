1149ef24e7d93593dc4c2768b0fea3883adbf294
Esa Sepänniemi <esa.sepanniemi@gmail.com>
2016-10-22 20:51:47 +0300

    Add support for git history reporting as html, json or md.
    Added maven plugin to create git history reports at build time
    Added support for loading git report files and endpoint to fetch them

--- src/main/java/com/sepanniemi/changelog/ChangeLogLoader.java
+++ src/main/java/com/sepanniemi/changelog/ChangeLogLoader.java
@@ -5,21 +5,47 @@ import org.apache.commons.io.IOUtils;
 import org.pegdown.PegDownProcessor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.DefaultResourceLoader;
 import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
 
 @Component
 @Getter
 public class ChangeLogLoader {
 
+    private static ResourceLoader resourceLoader = new DefaultResourceLoader();
+
     private String changeLogHtml;
 
+    private String changeLog;
+
+    private Map<String, String> gitChangeLogs = new HashMap<>();
+
     @Autowired
-    public ChangeLogLoader(@Value("${changelog.path}") Resource changeLog) throws IOException {
+    public ChangeLogLoader(@Value("${changelog.path}") Resource changeLogResource, @Value("${git.logs.path}")String gitLogsPath) throws IOException {
         PegDownProcessor pegDownProcessor = new PegDownProcessor();
-        changeLogHtml = pegDownProcessor.markdownToHtml(IOUtils.toString(changeLog.getInputStream(), StandardCharsets.UTF_8.name()));
+        changeLog = IOUtils.toString(changeLogResource.getInputStream(), StandardCharsets.UTF_8.name());
+        changeLogHtml = pegDownProcessor.markdownToHtml(changeLog);
+
+        //todo: make file names configurable, validate path
+        gitChangeLogs.put("html", loadResourceContents(gitLogsPath+ "git-changelog.html"));
+        gitChangeLogs.put("json", loadResourceContents(gitLogsPath+ "git-changelog.json"));
+        gitChangeLogs.put("html-table",  loadResourceContents(gitLogsPath+ "git-changelog-tableonly.html"));
+        gitChangeLogs.put("md", loadResourceContents(gitLogsPath+ "git-changelog.md"));
+
+    }
+
+    public String getGitChangelog(String type){
+        return gitChangeLogs.get(type);
+    }
+
+    private static String loadResourceContents(String resourcePath ) throws IOException {
+        return IOUtils.toString(resourceLoader.getResource(resourcePath).getInputStream());
     }
 }
--- src/main/java/com/sepanniemi/changelog/ChangeLogResource.java
+++ src/main/java/com/sepanniemi/changelog/ChangeLogResource.java
@@ -2,16 +2,18 @@ package com.sepanniemi.changelog;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @Slf4j
 @RequestMapping("/changelog")
 public class ChangeLogResource {
 
+    private static final String DEFAULT_GIT_LOG_TYPE = "html";
     private ChangeLogLoader changeLogLoader;
 
     @Autowired
@@ -21,7 +23,14 @@ public class ChangeLogResource {
 
     @RequestMapping(method = RequestMethod.GET)
     @ResponseBody
-    public String get(){
-        return changeLogLoader.getChangeLogHtml();
+    public String get(@RequestParam(value = "raw", required = false ) boolean raw) {
+        return raw ? changeLogLoader.getChangeLog() : changeLogLoader.getChangeLogHtml();
+    }
+
+
+    @RequestMapping(value = "/git", method = RequestMethod.GET)
+    @ResponseBody
+    public String getGit(@RequestParam(value = "type", required = false ) String type ) {
+        return changeLogLoader.getGitChangelog(type != null ? type : DEFAULT_GIT_LOG_TYPE );
     }
 }
