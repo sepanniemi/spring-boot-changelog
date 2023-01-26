169d4950716f482a67711e0a41bcd6733b965a2b
vrnsky <vrnsky@protonmail.com>
2023-01-26 12:40:55 +0800

    Spring Boot Changelog Plugin update

--- src/test/java/com/sepanniemi/changelog/SpringBootChangelogApplicationTests.java
+++ src/test/java/com/sepanniemi/changelog/SpringBootChangelogApplicationTests.java
@@ -1,16 +1,14 @@
 package com.sepanniemi.changelog;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
 @SpringBootTest
 public class SpringBootChangelogApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
 }
